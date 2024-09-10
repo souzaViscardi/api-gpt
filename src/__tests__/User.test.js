2b72a6602f9f5c47089942374632585c542667a3
Kevin  viscardi de souza <viscardi.souza@gmail.com>
2024-09-10 16:31:26 -0300

    feat: verify if user exist for create

--- src/__tests__/User.test.js
+++ src/__tests__/User.test.js
@@ -2,16 +2,24 @@ import UserService from '../services/User';
 import User from '../database/models/User.js';
 jest.mock('mongoose');
 const mockSave = jest.fn();
-const mockFindById = jest.fn();
+const mockFindOne = jest.fn();
 const mockFind = jest.fn();
 
-jest.mock('../database/models/User.js', () => {
+jest.mock('../database/models/User', () => {
+  const mockSave = jest.fn();
+  const mockFindById = jest.fn();
+  const mockFindOne = jest.fn();
+  const mockFind = jest.fn();
 
-  return jest.fn().mockImplementation(() => ({
-    findById: mockFindById,
-    find: mockFind,
+  const User = jest.fn().mockImplementation(() => ({
     save: mockSave,
   }));
+
+  User.findById = mockFindById;
+  User.findOne = mockFindOne;
+  User.find = mockFind;
+  User.prototype.save = mockSave
+  return User;
 });
 
 describe('UserService', () => {
@@ -26,22 +34,33 @@ describe('UserService', () => {
   });
 
   test('should create a user', async () => {
-    const userData = { id:"2132198390128312", name: 'John Deer', email: 'john.deer@example.com', phone: '123-456-7890' };
-    let user = new User()
-    user.save.mockResolvedValue(userData);
+    const userData = { id:"2132198390128312", name: 'John Deer', email: 'john.deer@example.com', phone: '1234567890' };
+    let email = null
+    User.prototype.save.mockResolvedValue(userData)
+    User.findOne.mockResolvedValue(email);
+
     const result = await userService.createUser(userData);
-    expect(user.save).toHaveBeenCalled();
+    expect(User.prototype.save).toHaveBeenCalled();
     expect(result).toEqual(userData);
+  });
+  
+  test('should verify if user exist', async () => {
+    const userData = { id:"2132198390128312", name: 'John Deer', email: 'john.deer@example.com', phone: '1234567890' };
+
+    let email = true
+    User.findOne.mockResolvedValue(email);
+
+    const result = await userService.createUser(userData);
+    expect(result).toEqual("User already registered");
   });
 
   test('should handle error when creating a user', async () => {
-    const userData = { id:"2132198390128312", name: 'John Deer', email: 'john.deer@example.com', phone: '123-456-7890' };
+    const userData = { id:"2132198390128312", name: 'John Deer', email: 'john.deer@example.com', phone: '1234567890' };
 
     const errorMessage = 'Failed to save user';
     let user = new User()
 
     user.save.mockRejectedValue(new Error(errorMessage));
-
     try {
       await userService.createUser(userData);
     } catch (error) {
